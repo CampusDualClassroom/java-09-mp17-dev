6d937cfa17758da2ecb7814d19d79c3e66315cc8
Julio Díaz <juliodiaz1792@gmail.com>
2024-09-27 11:44:18 +0200

    Solved exercise

--- src/main/java/com/campusdual/classroom/Exercise09.java
+++ src/main/java/com/campusdual/classroom/Exercise09.java
@@ -3,7 +3,7 @@ package com.campusdual.classroom;
 public class Exercise09 {
 
     public static void main(String[] args) {
-
+        doWhileLoop(8);
     }
 
     //TODO ↓
@@ -12,6 +12,10 @@ public class Exercise09 {
     // Entra con valor: X+1, sale con valor: X+2;
     // etc.
     public static void doWhileLoop(int num) {
-
+        int counter = 0;
+        do {
+            System.out.println("Entra con valor: " + counter + ", sale con valor: " + (counter + 1));
+            counter++;
+        } while (counter < num);
     }
 }
